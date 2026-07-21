d9f959b11efc2cc3dc46f83f6d5535fcee36ec25
loid345 <volanar@tut.by>
2026-07-21 12:19:03 +0300

    feat(forum): add category lifecycle entity

---
+++ crates/rustok-forum/src/entities/forum_category_lifecycle.rs
@@ -0,0 +1,33 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
+#[sea_orm(table_name = "forum_category_lifecycle")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub category_id: Uuid,
+    pub tenant_id: Uuid,
+    pub archived_at: DateTimeWithTimeZone,
+    pub updated_at: DateTimeWithTimeZone,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::forum_category::Entity",
+        from = "Column::CategoryId",
+        to = "super::forum_category::Column::Id",
+        on_update = "Cascade",
+        on_delete = "Cascade"
+    )]
+    Category,
+}
+
+impl Related<super::forum_category::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::Category.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
