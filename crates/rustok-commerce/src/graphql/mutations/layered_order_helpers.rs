fe0200526db1b19ef45f06ccf1280dcd73485293
loid345 <volanar@tut.by>
2026-07-27 22:50:39 +0300

    Restore compact typed helper exports

--- crates/rustok-commerce/src/graphql/mutations/layered_order_helpers.rs
+++ crates/rustok-commerce/src/graphql/mutations/layered_order_helpers.rs
@@ -1,46 +1,6 @@
-#![allow(hidden_glob_reexports)]
+#![allow(ambiguous_glob_reexports, hidden_glob_reexports)]
 
 pub(crate) use super::safe_order_helpers_impl::*;
-
-#[allow(clippy::too_many_arguments)]
-pub(crate) async fn resolve_storefront_line_item_input(
-    db: &sea_orm::DatabaseConnection,
-    tenant_id: uuid::Uuid,
-    pricing_read_port: &dyn rustok_pricing::PricingReadPort,
-    pricing_port_context: rustok_api::PortContext,
-    pricing_context: &rustok_pricing::PriceResolutionContext,
-    locale: &str,
-    default_locale: &str,
-    public_channel_slug: Option<&str>,
-    input: super::super::types::AddStorefrontCartLineItemInput,
-) -> async_graphql::Result<super::legacy_helpers::ResolvedStorefrontLineItemInput> {
-    super::typed_line_item_helpers::resolve_storefront_line_item_input(
-        db,
-        tenant_id,
-        pricing_read_port,
-        pricing_port_context,
-        pricing_context,
-        locale,
-        default_locale,
-        public_channel_slug,
-        input,
-    )
-    .await
-}
-
-pub(crate) async fn validate_storefront_line_item_quantity(
-    db: &sea_orm::DatabaseConnection,
-    tenant_id: uuid::Uuid,
-    variant_id: uuid::Uuid,
-    requested_quantity: i32,
-    public_channel_slug: Option<&str>,
-) -> async_graphql::Result<()> {
-    super::typed_line_item_helpers::validate_storefront_line_item_quantity(
-        db,
-        tenant_id,
-        variant_id,
-        requested_quantity,
-        public_channel_slug,
-    )
-    .await
-}
+pub(crate) use super::typed_line_item_helpers::{
+    resolve_storefront_line_item_input, validate_storefront_line_item_quantity,
+};
