63021b6321b5726814702314740b38476e381d83
loid345 <volanar@tut.by>
2026-07-27 22:31:11 +0300

    Allow intentional layered helper overrides

--- crates/rustok-commerce/src/graphql/mutations/layered_order_helpers.rs
+++ crates/rustok-commerce/src/graphql/mutations/layered_order_helpers.rs
@@ -1,3 +1,5 @@
+#![allow(ambiguous_glob_reexports, hidden_glob_reexports)]
+
 pub(crate) use super::safe_order_helpers_impl::*;
 pub(crate) use super::typed_line_item_helpers::{
     resolve_storefront_line_item_input, validate_storefront_line_item_quantity,
