5c9cd064f9bd649fdb0fbd872ff53b733e5b371c
loid345 <volanar@tut.by>
2026-08-06 10:30:02 +0300

    feat(index): mount sealed source-page diagnosis mutation

--- apps/server/src/graphql/schema.rs
+++ apps/server/src/graphql/schema.rs
@@ -20,6 +20,7 @@ mod schema_codegen {
 use super::dashboard_security::GraphqlDashboardSecurityPolicy;
 use super::forum_principal_security::ForumPrincipalPolicy;
 use super::index_drift_diagnosis::IndexDriftDiagnosisMutation;
+use super::index_drift_source_page_diagnosis::IndexDriftSourcePageDiagnosisMutation;
 use super::legacy_disable_user::LegacyDisableUserPolicy;
 use super::loaders::TenantNameLoader;
 use super::module_security::GraphqlModuleSecurityPolicy;
@@ -84,6 +85,7 @@ pub struct Query(
 pub struct Mutation(
     RootMutation,
     IndexDriftDiagnosisMutation,
+    IndexDriftSourcePageDiagnosisMutation,
     #[cfg(all(
         feature = "mod-content",
         feature = "mod-blog",
