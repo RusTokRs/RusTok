f78a11ac94c5a05f02d3fa867108e62ecd6de0af
loid345 <volanar@tut.by>
2026-09-14 20:21:35 +0300

    feat(rbac): add presentation write GraphQL contract

--- crates/modules/rustok-rbac/src/graphql/types.rs
+++ crates/modules/rustok-rbac/src/graphql/types.rs
@@ -1,5 +1,7 @@
-use async_graphql::{Enum, SimpleObject};
+use async_graphql::{Enum, InputObject, SimpleObject};
 use rustok_core::UserRole;
+
+use crate::RbacPresentationResourceKind;
 
 #[derive(Enum, Copy, Clone, Debug, Eq, PartialEq)]
 #[graphql(rename_items = "SCREAMING_SNAKE_CASE")]
@@ -19,6 +21,42 @@ impl From<RbacGraphqlUserRole> for UserRole {
             RbacGraphqlUserRole::Customer => UserRole::Customer,
         }
     }
+}
+
+#[derive(Enum, Copy, Clone, Debug, Eq, PartialEq)]
+#[graphql(rename_items = "SCREAMING_SNAKE_CASE")]
+pub enum RbacGraphqlPresentationResourceKind {
+    Role,
+    Permission,
+}
+
+impl From<RbacGraphqlPresentationResourceKind> for RbacPresentationResourceKind {
+    fn from(kind: RbacGraphqlPresentationResourceKind) -> Self {
+        match kind {
+            RbacGraphqlPresentationResourceKind::Role => Self::Role,
+            RbacGraphqlPresentationResourceKind::Permission => Self::Permission,
+        }
+    }
+}
+
+#[derive(InputObject, Debug, Clone)]
+pub struct WriteRbacPresentationInput {
+    pub resource_kind: RbacGraphqlPresentationResourceKind,
+    pub resource_key: String,
+    pub locale: String,
+    pub expected_copy_revision: Option<i64>,
+    pub name: String,
+    pub description: Option<String>,
+}
+
+#[derive(Debug, Clone, SimpleObject)]
+pub struct RbacPresentationPayload {
+    pub resource_kind: RbacGraphqlPresentationResourceKind,
+    pub resource_key: String,
+    pub locale: String,
+    pub name: String,
+    pub description: Option<String>,
+    pub copy_revision: i64,
 }
 
 #[derive(Debug, Clone, SimpleObject)]
