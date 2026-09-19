8ae430ecb419fbb11a469ff9194ca4b60f00016f
loid345 <volanar@tut.by>
2026-09-19 06:18:03 +0300

    refactor(blog): remove duplicate category hierarchy lock

--- crates/modules/rustok-blog/src/services/category_command.rs
+++ crates/modules/rustok-blog/src/services/category_command.rs
@@ -170,24 +170,6 @@ impl CategoryCommandService {
     }
 }
 
-async fn lock_category_tree_in_tx(txn: &DatabaseTransaction, tenant_id: Uuid) -> BlogResult<()> {
-    match txn.get_database_backend() {
-        DatabaseBackend::Postgres => {
-            txn.execute_raw(Statement::from_sql_and_values(
-                DatabaseBackend::Postgres,
-                "SELECT pg_advisory_xact_lock(hashtextextended($1, 0))",
-                [format!("blog-category-tree:{tenant_id}").into()],
-            ))
-            .await?;
-            Ok(())
-        }
-        DatabaseBackend::Sqlite => Ok(()),
-        backend => Err(BlogError::invariant(format!(
-            "Blog category hierarchy commands do not support storage backend {backend:?}"
-        ))),
-    }
-}
-
 async fn load_categories_in_tx(
     txn: &DatabaseTransaction,
     tenant_id: Uuid,
